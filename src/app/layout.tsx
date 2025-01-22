59f4b4d11f6e5f97db4c4686bc7872c5a70394a5
Somdev Behera <64832169+somdev-hub@users.noreply.github.com>
2025-01-22 11:33:46 +0530

    Add Google site verification meta tag in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="ironeutXhmCnTSBbPPyeJKxKZTWhqypUM8ssFKquPz8" />
+      </head>
       <body className="antialiased bg-[#eeeeee]">
         <Suspense fallback={<Loading />}>
           <SidebarProvider>
